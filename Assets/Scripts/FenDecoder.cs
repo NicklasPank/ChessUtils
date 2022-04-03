8e0355cbab8a53e690678b8258e9a0526b86e1ec
npank3 <npank3@gatech.edu>
2022-04-03 16:59:38 -0400

    fen decoder w/ initial pos

--- Assets/Scripts/FenDecoder.cs
+++ Assets/Scripts/FenDecoder.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class FenDecoder : MonoBehaviour
 {
-    // Start is called before the first frame update
+    PieceCreator pc;
+    string startingFen;
     void Start()
     {
-        Debug.Log("Hi");
+        translate(decode(startingFen));
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        pc = gameObject.GetComponent<PieceCreator>();
+        startingFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+    }
+    void translate(int[] decoded)
+    {
+        pc.clear();
+
+        for (int i = 0; i < decoded.Length; i++)
+        {
+            if (decoded[i] == -1) { continue; }
+            int file = i % 8;
+            int rank = i / 8;
+            bool isWhite = decoded[i] < 8;
+            int type = decoded[i] & 7;
+            pc.add(isWhite, type, file, rank);
+        }
+    }
+    int[] decode(string fen)
+    {
+        int[] decoded = new int[64];
+        string wpieces = "PNBRKQ";
+        string bpieces = "pnbrkq";
+        string nums = "12345678";
+        int ind = 56;
+        for (int i = 0; i < fen.Length; i++)
+        {
+            string c = fen[i] + "";
+            if (wpieces.Contains(c))
+            {
+                decoded[ind] = wpieces.IndexOf(c);
+                ind++;
+            }
+            else if (bpieces.Contains(c))
+            {
+                decoded[ind] = bpieces.IndexOf(c) + 8;
+                ind++;
+            }
+            else if (nums.Contains(c))
+            {
+                for (int j = 0; j < nums.IndexOf(c) + 1; j++)
+                {
+                    decoded[ind] = -1;
+                    ind++;
+                }
+            }
+            else if (c == "/")
+            {
+                ind -= 16;
+            }
+            else
+            {
+                Debug.Log("Invalid fen of: " + fen);
+                return null;
+            }
+        }
+
+        return decoded;
     }
 }
